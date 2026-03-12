ce3e6cb0f649ddd10446c155d80d8d3f9230fc66
Miguelanguelo <153223458+Miguelanguelo@users.noreply.github.com>
2026-03-12 12:45:30 +0000

    commit

--- batalha_naval.c
+++ batalha_naval.c
@@ -5,6 +5,16 @@ int main() {
 int tabu [10] [10] = {0};
 char colunas [10] = {'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J'};
 
+for(int i = 3; i < 6; i++) {
+      
+        tabu [i] [2] = 3;    
+}
+
+for(int i = 5; i <= 7; i++) {
+      
+        tabu [8] [i] = 3;    
+}
+
 printf("     ");
   
 for (int i = 0; i <= 9  ; i++) {
@@ -15,10 +25,10 @@ printf(" %c " , colunas [i]);
 
 printf("\n");
 
-for(int i = 0 , h = 1; i <= 10 && h <= 10; i++ , h++) {
+for(int i = 0; i < 10; i++) {
     
     printf("\n");
-    printf("%2d | " , h);
+    printf("%2d | " , i+1);
 
     for(int j = 0 ; j < 10; j++) {
 
